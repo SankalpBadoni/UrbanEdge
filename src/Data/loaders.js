d9bdf91bd4b99457ecc5bbf0e284cdf7fcc49a5b
sankalp <badonisankalp123@gmail.com>
2025-02-06 23:38:22 +0530

    made loader for list page

--- src/Data/loaders.js
+++ src/Data/loaders.js
@@ -8,3 +8,16 @@ export const singlePageLoader = async ({ params }) => {
         throw new Response("Property not found", { status: error.response?.status || 500 });
     }
 };
+
+export const listPageLoader = async ({request, params }) => {
+    // console.log(request)
+    const query = request.url.split("?")[1]
+    const res = await apiRequest.get(`/properties?${query}`); 
+    return res.data; 
+    // try {
+    //     const res = await apiRequest.get(`/properties/${params.id}`); 
+    //     return res.data; 
+    // } catch (error) {
+    //     throw new Response("Property not found", { status: error.response?.status || 500 });
+    // }
+};
--- src/Pages/ListPage/ListPage.jsx
+++ src/Pages/ListPage/ListPage.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { listData } from '../../Data/DummyData'
-import Filter from '../../Components/Filter/Filter'
-import Card from '../../Components/Card/Card'
-import Map from '../../Components/Map/Map'
+import React from "react";
+import { listData } from "../../Data/DummyData";
+import Filter from "../../Components/Filter/Filter";
+import Card from "../../Components/Card/Card";
+import Map from "../../Components/Map/Map";
+import { useLoaderData } from "react-router-dom";
 
 export default function ListPage() {
-    const data = listData
+  const data = listData;
+  const propertiesData = useLoaderData();
+  const properties = propertiesData?.properties || []; 
+  console.log("loader data", properties);
   return (
     <>
-        <div className='flex h-screen'>
-            <div className='w-3/5 pr-12 flex flex-col overflow-y-auto'>
-              <Filter/>
-              <div className='flex flex-col gap-12 overflow-y-auto'>
-              {data.map(item => (
-                  <Card key = {item.id} item={item}/> //passing item as a prop so that we can access item.properties
-              ))}
-              </div>
-              
-            </div>
-            
-            <div className='w-2/5 bg-pink-100'>
-              <Map items={data}/>
-            </div>
+      <div className="flex h-screen">
+        <div className="w-3/5 pr-12 flex flex-col overflow-y-auto">
+          <Filter />
+          <div className="flex flex-col gap-12 overflow-y-auto">
+            { Array.isArray(properties) && properties.length > 0 ? (
+              properties.map((item) => <Card key={item.id} item={item} />)
+            ) : (
+              <p className="text-gray-500">No properties found.</p>
+            )}
+          </div>
         </div>
+
+        <div className="w-2/5 bg-pink-100">
+          <Map items={properties} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
