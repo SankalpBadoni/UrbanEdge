9e187fdac5471f848817eceb3482d099bbf211a6
sankalp <badonisankalp123@gmail.com>
2025-06-29 17:58:37 +0530

    updated profile page url

--- src/Data/ApiRequest.js
+++ src/Data/ApiRequest.js
@@ -4,5 +4,16 @@ const apiRequest = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
-
+apiRequest.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default apiRequest;
